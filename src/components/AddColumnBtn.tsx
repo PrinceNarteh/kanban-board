2d1db76bcad1d81fe2b35a48d087453492dbe98a
Prince Narteh <prinarteh@gmail.com>
2024-02-21 12:57:24 +0000

    feat: add icons

--- src/components/AddColumnBtn.tsx
+++ src/components/AddColumnBtn.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { PlusIcon } from "../icons/PlusIcon";
 
 type AddColumnBtnProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const AddColumnBtn: React.FC<AddColumnBtnProps> = () => {
   return (
-    <button className="h-14 w-60 min-w-60 cursor-pointer bg-primary rounded-lg border-2 border-secondary ring-rose-500 hover:ring-2 duration-500">
-      Add Column
+    <button className="flex gap-3 justify-center items-center h-14 w-60 min-w-60 cursor-pointer bg-primary rounded-lg border-2 border-secondary ring-rose-500 hover:ring-2 duration-500">
+      <PlusIcon />
+      <span>Add Column</span>
     </button>
   );
 };
