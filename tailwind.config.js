dcb5e16b8c42aa24675bcfbbd2018b6babf9e409
Prince Narteh <prinarteh@gmail.com>
2024-02-21 11:50:24 +0000

    feat: done with setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0D1117",
+        secondary: "#161C22",
+      },
+    },
   },
   plugins: [],
 };
